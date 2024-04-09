7e4352b5d8fb7738a460fae1a1b1baa9088ea2ab
Sam Judd <sam@convex.dev>
2024-04-08 17:49:46 -0700

    Export a few missing backfill types.

--- src/lib.rs
+++ src/lib.rs
@@ -42,7 +42,7 @@ mod error;
 mod serde;
 mod util;
 
-pub use client::backfill::CreateBackfillParams;
+pub use client::backfill::{BackfillStatus, CreateBackfillParams, BackfillStatusResponse, ListBackfillsResponse};
 pub use client::coupons::{Coupon, CouponListParams, Discount, RedeemedCoupon};
 pub use client::customers::{
     AddIncrementCreditLedgerEntryRequestParams, AddVoidCreditLedgerEntryRequestParams, Address,
